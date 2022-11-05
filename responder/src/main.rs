fdb2091242ccacebb8880f93d583a243de16066c
HackingGuy <v.samuel88@yahoo.com>
2022-11-05 13:03:36 -0500

    add rust server base testing

--- responder/src/main.rs
+++ responder/src/main.rs
@@ -1,7 +1,29 @@
-use std::io;
+use std::env;
+use std::io::*;
+use std::process::{Command, Stdio};
 
-fn main() -> io::Result<()> {
-    let input = io::read_to_string(io::stdin())?;
-    println!("{input}");
-    io::Result::Ok(())
+fn main() -> std::io::Result<()> {
+    let input: Vec<String> = env::args().collect();
+    dbg!(input);
+    Ok(())
+}
+
+fn send_packet(node: String, packet: String) -> Result<()> {
+    let mut child = Command::new(format!("dtnsend -r dtn://{node}"))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn child process");
+
+    let mut stdin = child.stdin.take().expect("Failed to open stdin");
+    std::thread::spawn(move || {
+        stdin
+            .write_all(packet.as_bytes())
+            .expect("Failed to write to stdin");
+    });
+
+    let output = child.wait_with_output().expect("Failed to read stdout");
+    println!("This is the output of the child process: {output:?}");
+
+    Ok(())
 }

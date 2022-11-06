c5f0e8ccae29ec0d7e3ee168ea2f6d3cade3c503
Victor Wei <vw1262@gmail.com>
2022-11-06 07:40:18 -0600

    fix python pilot file

--- pilot/main.py
+++ pilot/main.py
@@ -1,6 +1,8 @@
 from microbit import *
 
+uart.init(115200)
 while True:
-    print(microbit.accelerometer.get_values())
+    data = accelerometer.get_strength()
+    uart.write("Accl (milli-g): " + str(data))
+    uart.write("\r")
     sleep(1000)
-
